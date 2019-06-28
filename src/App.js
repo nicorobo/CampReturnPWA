11b382c083b3ae1f55a4c371a9e5b04efb51db46
nickroberts404 <nickroberts404@gmail.com>
2019-06-28 09:40:25 -0500

    Initial artist list/app structure

--- src/App.js
+++ src/App.js
@@ -1,26 +1,77 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Router, Link } from '@reach/router';
+import data from './data.js';
+import { link } from 'fs';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="app">
+			<Router>
+				<Artists path="artists" />
+				<Artist path="artists/:slug" />
+			</Router>
+		</div>
+	);
 }
+
+const Artists = () => (
+	<div className="artists">
+		<h3>Artists</h3>
+		<ArtistList />
+	</div>
+);
+
+const Artist = ({ slug }) => {
+	const { id, title, photo, bio, links } = data.artists.find((a) => a.slug === slug);
+	return (
+		<div className="artist">
+			<img src={photo} />
+			<h3>{title}</h3>
+			<div>
+				{links.soundcloud && <a href={links.soundcloud}>Soundcloud</a>}
+				{links.facebook && <a href={links.facebook}>Facebook</a>}
+				{links.instagram && <a href={links.instagram}>Instagram</a>}
+				{links.ra && <a href={links.ra}>Resident Advisor</a>}
+			</div>
+			<SetList id={id} />
+			<p>{bio}</p>
+		</div>
+	);
+};
+
+const SetList = ({ id }) => {
+	const events = data.events.filter((e) => e.hosts.indexOf(id) >= 0);
+	return (
+		<div>
+			{events.map((e) => (
+				<SetItem event={e} />
+			))}
+		</div>
+	);
+};
+
+const SetItem = ({ event }) => {
+	return <div>{data.locations[event.location].title}</div>;
+};
+
+const ArtistList = () => (
+	<div className="artist-list">
+		{data.artists.map((a) => (
+			<ArtistItem key={a.id} artist={a} />
+		))}
+	</div>
+);
+
+const ArtistItem = ({ artist }) => {
+	const { slug, title, photo } = artist;
+	return (
+		<Link to={'/artists/' + slug}>
+			<div className="artist-item">
+				<img className="artist-photo" src={photo} />
+				<div className="artist-title">{title}</div>
+			</div>
+		</Link>
+	);
+};
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,91 @@
+const data = {
+	artists: [
+		{
+			id: 0,
+			slug: 'notmysnakehead',
+			title: 'notmysnakehead',
+			bio:
+				"Nick has been tickling guitar strings since age seven, but was introduced to the terrifying magic of electronic music in 2016. That party in the desert addicted him to exploring all sounds ancient and all grooves new. Come prepared to get down stay down, or at least jiggle gently, to the funky sounds of our deserts, jungles, and islands. When he's not laying it down fat behind the decks, you'll find him shuffling jiffy pop near the fire. ",
+			photo:
+				'https://scontent.faus1-1.fna.fbcdn.net/v/t1.0-9/62035198_627843697729490_2099188634837057536_o.jpg?_nc_cat=110&_nc_oc=AQnWZ6K2k6T6JbYiN0QgTvCjweS6Tsyq1L5pRrzwxGgB0td3kNWHzAkTb3OP8JUQ-K143VUGZX6C5l0XA0I5bTT6&_nc_ht=scontent.faus1-1.fna&oh=078128b68250156f9e23285e6c422c2a&oe=5DBF00F0',
+			links: {
+				instagram: '',
+				soundcloud: 'http://soundcloud.com/notmysnakehead',
+			},
+		},
+		{
+			id: 1,
+			slug: 'trad-dad',
+			title: 'Trad Dad',
+			bio:
+				"Past: In a world where Silence spreads across the universe laying waste to everything in its path…two alien brothers, Karen Slater and Cobra Draven, strive to maintain the balance of the universe by spreading intergalactic sound waves capable of moving the cosmos. As Sound Fighters for the Gressefician Federation, they were on the front lines when the dark Silence reached the Alpha Centauri system. Sadly, they could not stop the destruction of their home planet, Renegaada. Forced to flee, they traveled to the Solar System, settling on a planet called Earth. The year was 1988. The brothers immersed themselves in their new home, studying human cultures and histories, art, music and all manner of worldly things. They adopted humanoid ways, customs and traditions. They even picked up camping and climbing (it was probably due to all the shiny gear). Present: Many moons, seasons of the Simpsons and hairstyles have passed, but one thing remains true: the battle isn't over. Relying on their intergalactic know-how, they mix nostalgic selections and spice up the night with a goofy, dad-like presence. The Sound Fighters vow to uphold their oaths and bring the Boogie.",
+			photo:
+				'https://scontent.faus1-1.fna.fbcdn.net/v/t1.0-9/61612869_628530364327490_2619865969647943680_o.jpg?_nc_cat=103&_nc_oc=AQlGGgMctfd-GLYrYq7DV01ymTrJ_H8_rC9uAhZ2oYBcGdhmMXxTrboSZEAReSu6SvcPAwBQgtlniDzsNaoIu5mv&_nc_ht=scontent.faus1-1.fna&oh=25e2507d0334473c6c4bd4048f59578b&oe=5D847181',
+			links: {
+				instagram: '',
+				soundcloud: '',
+			},
+		},
+		{
+			id: 2,
+			slug: 'director-of-vibe',
+			title: 'Director of Vibe',
+			bio:
+				'Ryan has found himself within an ever evolving wormhole of deep melodic house, electro, and dark dirty progressive techno. He is inspired by a combination of the sounds of Berlin and the west coast transformational festival scene. He is tall, awkward, real as fuck, and you’ll probably see him running around stressing out over minor details as well as mashing buttons in the back of the Peak Stage playing with lighting. Each and everyone of you having a phenomenal time at Camp Return will spark joy in his heart. You can catch him playing throwing down the dark dirty goodness at the Peak stage Friday night and then some more melodic jams in La Renegaada on Saturday. Come get weird! ',
+			photo:
+				'https://scontent.faus1-1.fna.fbcdn.net/v/t1.0-9/61641496_626527677861092_2446252473736757248_n.png?_nc_cat=109&_nc_oc=AQlJvzLYMIwuJ1i-W-l1w9psvdsloHUovP52gY5_YV555-KvvwidsPPR2zugtNJJ1pSNgm2bfOBQVYg_K9YojbVi&_nc_ht=scontent.faus1-1.fna&oh=a80b045f9bd7ae7fc6a17f194dbb60fb&oe=5DC53639',
+			links: {
+				instagram: '',
+				soundcloud: 'http://soundcloud.com/directorofvibe',
+			},
+		},
+		{
+			id: 3,
+			slug: 'erik-vehmeyer',
+			title: 'Erik Vehmeyer',
+			bio:
+				"Peak Stage's lead designer and musical curator Erik Vehmeyer is a growing musical force in local circles. As a member of Like.Minded affiliate of collectives including For the Love and Chinatown Sessions, his presence on the local underground circuit has steadily increased since Camp Return’s inception. Known for pushing boundaries between styles and genres, his adventurous selections are sure to keep you intrigued. Weaving through acid-tinged Italo disco, psychedelic-rock infused electro and spaced-out breakbeat techno, every set becomes a journey through space and time. With a crate of 90’s chicago house and breakbeat records, Vehmeyer chose to start from ground zero; learning the dying art of vinyl mixing before ever touching digital equipment. Time spent honing his skills on the decks was matched with delving deep into studies on dance music history. Eager to explore and share sounds from the past and future, Erik’s sets pay homage to forgotten sounds and cutting-edge productions alike.",
+			photo:
+				'https://scontent.faus1-1.fna.fbcdn.net/v/t1.0-9/61542629_625831331264060_264684966003081216_o.png?_nc_cat=101&_nc_oc=AQk6ZgWcSdUgLri7Zj9xHPbyaiBmB_BdDIwjE9xvPSxPtXLNsU5lHyFK2rqpflgh_j9wbrIrki51tOXRvvF18tMX&_nc_ht=scontent.faus1-1.fna&oh=b1a52a7c8bd06e42e67cdbe9672b086f&oe=5DBCBFE6',
+			links: {
+				instagram: '',
+				soundcloud: 'https://soundcloud.com/erik-vehmeyer',
+				ra: 'https://www.residentadvisor.net/dj/erikvehmeyer',
+			},
+		},
+		{
+			id: 4,
+			slug: 'nicolas-pera',
+			title: 'Nicolas Pera',
+			bio:
+				'Unafraid to walk down new paths, Nicolas Pera has launched himself on a journey around the globe in search of novel sounds to augment his productions. He blends his musical influences into a sound that intensifies the atmosphere, with rich textures layered over hypnotic percussion. Pera is a member of the collective and label Sonido Trópico, well-known players in the underground scene in São Paulo, Brazil. We are stoked to have him back for another Camp Return Party!',
+			photo:
+				'https://scontent.faus1-1.fna.fbcdn.net/v/t1.0-9/61679684_628530314327495_4689962712225021952_n.png?_nc_cat=101&_nc_oc=AQm3EI8IhCtRKjPMFiR2mOntS0OQkC9dzLMP4MFh9KYaSwNWeokHX0ulUgM4p_Vs_hiQnLevnzTWodl8cZ7mDqxW&_nc_ht=scontent.faus1-1.fna&oh=ef5911b5d03a5579c11ed2938217381a&oe=5D851505',
+			links: {
+				instagram: '',
+				soundcloud: 'https://soundcloud.com/nicolaspera',
+			},
+		},
+	],
+	locations: [
+		{ title: 'Peak Stage', bio: '' },
+		{ title: 'Renegadda', bio: '' },
+		{ title: 'J-Hole', bio: '' },
+		{ title: 'Geodome', bio: '' },
+	],
+	events: [
+		{ type: 'music', hosts: [0], location: 1, time: [1559923200, 1559930400] },
+		{
+			type: 'workshop',
+			hosts: [5],
+			location: 1,
+			title: 'For Sure Yoga',
+			description: 'dkld dfkl dfklfd',
+			time: [1559923200, 1559930400],
+		},
+		{ type: 'music', hosts: [3], location: 0, time: [1559923200, 1559930400] },
+		{ type: 'music', hosts: [4], location: 1, time: [1559923200, 1559930400] },
+	],
+};
+
+export default data;
